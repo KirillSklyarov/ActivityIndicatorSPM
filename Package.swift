9a0d0fd3442484533dfc06c16ad154a6bdc7440d
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-01-16 13:44:24 +0300

    fix: minor bugs

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .target(
             name: "ActivityIndicatorSPM",
             resources: [
-                .process("Resources/Fonts")
+                .copy("Resources/Fonts")
             ]
         ),
     ]
