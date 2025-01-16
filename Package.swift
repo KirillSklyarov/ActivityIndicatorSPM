405c57904589e80d714beae2062453bce99e00d3
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-01-16 19:13:18 +0300

    testing

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppUIComponents",
+    name: "AppUIComponentsSPM",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AppUIComponents",
-            targets: ["AppUIComponents"]),
+            name: "AppUIComponentsSPM",
+            targets: ["AppUIComponentsSPM"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppUIComponents",
+            name: "AppUIComponentsSPM",
             resources: [
                 .copy("Resources/Fonts")
             ]
