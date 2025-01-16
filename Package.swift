5d609c8a0a13e1177fd7b48bed7e15a0bb7026f6
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-01-16 18:36:42 +0300

    feat: rename path

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "ActivityIndicatorSPM",
+    name: "AppUIComponents",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ActivityIndicatorSPM",
-            targets: ["ActivityIndicatorSPM"]),
+            name: "AppUIComponents",
+            targets: ["AppUIComponents"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ActivityIndicatorSPM",
+            name: "AppUIComponents",
             resources: [
                 .copy("Resources/Fonts")
             ]
