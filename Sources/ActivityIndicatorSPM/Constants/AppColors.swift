35f55c3f77700394ae93cb0328e8637f7fd83c6b
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-01-16 18:12:19 +0300

    fix: minor bugs

--- Sources/ActivityIndicatorSPM/Constants/AppColors.swift
+++ Sources/ActivityIndicatorSPM/Constants/AppColors.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 public struct AppColors {
-    static let backgroundGray = UIColor(hex: "242424")
-    static let backgroundBlack = UIColor(hex: "171717")
-    static let buttonGray = UIColor(hex: "363636")
-    static let dodoCoinsBlue = UIColor(hex: "5f4eca")
-    static let buttonOrange = UIColor(hex: "ff6400")
-    static let grayFont = UIColor(hex: "727272")
-    static let sberGreen = UIColor(hex: "06c906")
-    static let detailsBackground = UIColor(hex: "485460")
+    public static let backgroundGray = UIColor(hex: "242424")
+    public static let backgroundBlack = UIColor(hex: "171717")
+    public static let buttonGray = UIColor(hex: "363636")
+    public static let dodoCoinsBlue = UIColor(hex: "5f4eca")
+    public static let buttonOrange = UIColor(hex: "ff6400")
+    public static let grayFont = UIColor(hex: "727272")
+    public static let sberGreen = UIColor(hex: "06c906")
+    public static let detailsBackground = UIColor(hex: "485460")
 }
